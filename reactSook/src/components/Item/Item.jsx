6f94d2f3e088ba2b0ab669078dab550bdf045b14
orianasosa <sosa.a.ori@gmail.com>
2023-03-22 18:22:21 -0300

    estilos cards

--- reactSook/src/components/Item/Item.jsx
+++ reactSook/src/components/Item/Item.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom"
-import styles from "../Item/item.module.css"
+import styles from "../Item/item.css"
 
 const Item = ({producto}) => {
     return (
-        <Link to={`productos/${producto.id}`} className="col-md-3 text-center">
-            <img src={producto.imagen} className={styles.imagen} alt="" />
+        <Link to={`productos/${producto.id}`} className="col-md-3 text-center link">
+            <img src={producto.imagen} className="img" alt="" />
             <h3>{producto.nombre}</h3>
             <h5>{producto.categoria}</h5>
             <p>${producto.precio}</p>

f37afabc0832fa824abc55b6daa8d263b8cdf62e
orianasosa <sosa.a.ori@gmail.com>
2023-04-12 10:32:48 -0300

    pruebas

--- reactSook/src/components/ButtonAdd/ButtonAdd.jsx
+++ reactSook/src/components/ButtonAdd/ButtonAdd.jsx
@@ -1,10 +1,27 @@
+import { useContext } from "react"
+import { QuantityContext } from "../../contexts/QuantitySelectorContext"
+import { collection, doc, getDoc } from "firebase/firestore"
+import { useState } from "react"
+import { db } from "../../../db/firebase-config"
 
 
 const ButtonAdd = () => {
 
+    const [productos, setProductos] = useState([])
+
+    const addProductos = async (id) =>{
+        const productRef = doc(db, "productos", id)
+        const productSnap = await getDoc(productRef)
+        if(productSnap.exists()){
+            const obj = productos.find(x => x.id === (id))
+            setProductos(...productos, id)
+        } 
+    }
+
+        console.log(productos)
     return (
         <div>
-            <button>Agregar al carrito</button>
+            <button onClick={addProductos}>Agregar al carrito</button>
         </div>
     )
 }
--- reactSook/src/contexts/CartContext.jsx
+++ reactSook/src/contexts/CartContext.jsx
@@ -5,8 +5,20 @@ import { db } from "../../db/firebase-config";
 const CartContext = createContext()
 
 function CartProvider (props) {
-    
+    const [cart, setCart] =useState([])
+    const cartRef = collection(db, "cart")
 
+    const getCart = async () =>{
+        const cartCollection = await getDocs(cartRef)
+        const compras = cartCollection.docs.map((doc)=>({...doc.data(), id:doc.id}))
+        setCart(compras)
+    } 
+
+    return(
+        <CartContext.Provider value={{cart, getCart}}>
+            {props.children}
+        </CartContext.Provider>
+    )
 }
 
 export {CartContext, CartProvider}
